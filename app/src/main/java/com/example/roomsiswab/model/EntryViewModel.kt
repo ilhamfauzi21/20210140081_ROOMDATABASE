dda54de728a4f1eb2b0b34e741fe47887bc7d3c2
Ilham Fauzi <ilham.fauzi.ft21@mail.umy.ac.id>
2023-12-06 12:26:09 +0700

    add data class UIStateSiswa

--- app/src/main/java/com/example/roomsiswab/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswab/model/EntryViewModel.kt
@@ -1,5 +1,13 @@
 package com.example.roomsiswab.model
 
+
+
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
 data class DetailSiswa(
     val id: Int = 0,
     val nama: String = "",
