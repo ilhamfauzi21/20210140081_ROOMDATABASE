8f6a670fdbfbda48853c6047494b0c922b1f96de
Ilham Fauzi <ilham.fauzi.ft21@mail.umy.ac.id>
2023-12-06 12:55:21 +0700

    Membuat Interface DestinasiNavigasi code

--- app/src/main/java/com/example/roomsiswab/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/roomsiswab/navigasi/DestinasiNavigasi.kt
@@ -1,4 +1,14 @@
 package com.example.roomsiswab.navigasi
 
 interface DestinasiNavigasi {
+
+    /**
+     * Nama unik untuk menentukan jalur untuk composable
+     */
+    val route: String
+
+    /**
+     * String resource id yang berisi judul akan ditampilkan dilayar halaman.
+     */
+    val titleRes: Int
 }
